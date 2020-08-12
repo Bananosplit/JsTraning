9f884d2f860fc441092077a16ba8dfd699ba81cf
Bananosplit <Serg-rudk@yandex.ru>
2020-08-12 21:17:25 +0300

    Вынес класс Сервис в отдельный фаил

---
+++ star-wiki/src/servises/swapi-servises.js
@@ -0,0 +1,41 @@
+export default class  SwapiServise {
+
+    _apiBase = 'https://swapi.dev/api'
+  
+    async getResourse(url){
+       const res = await fetch(`${this._apiBase}${url}`)
+       if(!res.ok){
+         throw new Error(`Could not fetch ${url}, resived ${res.status}`)
+       }
+  
+       return await res.json()
+    }
+  
+   async getAllPeople(){
+      const resp = await this.getResourse(`/people/`)
+      return  resp.results
+    }
+  
+    getPersone(id){
+      return this.getResourse(`/people/${id}`)
+    }
+  
+    async getAllPlanets(){
+      const resp = await this.getResourse(`/planets/`)
+      return  resp.results
+    }
+  
+    getPlanet(id){
+      return this.getResourse(`/planets/${id}`)
+    }
+  
+    async getAllPStarships(){
+      const resp = await this.getResourse(`/starships/`)
+      return  resp.results
+    }
+  
+    getPlanet(id){
+      return this.getResourse(`/starships/${id}`)
+    }
+  
+  }
