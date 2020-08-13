089af8785597c136a8efbaaa364579c655a5adbc
Bananosplit <Serg-rudk@yandex.ru>
2020-08-13 21:56:19 +0300

    изменил название классов и чучуть стилистики

--- star-wiki/src/components/random-planet/random-planet.js
+++ star-wiki/src/components/random-planet/random-planet.js
@@ -8,22 +8,21 @@ export default function RandomPlanet() {
             <div className="planet-logo">
                 <img src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
             </div>
-            <div className='random-planet-wrap'>
+            <div className='random-planet-details'>
                 <div className="name-planet"> <h3>Name planet</h3></div>
-                <div className="list">
-
+                <div className="feature-list">
                     <ul>
-                        <li className='my-list'>
+                        <li className='feature-list-item'>
                             <span>Population</span>
                             <span>3000000</span>
                         </li>
 
-                        <li className='my-list'>
+                        <li className='feature-list-item'>
                             <span>Rotation Period</span>
                             <span>43</span>
                         </li>
 
-                        <li className='my-list'>
+                        <li className='feature-list-item'>
                             <span>Diameter</span>
                             <span>100</span>
                         </li>
