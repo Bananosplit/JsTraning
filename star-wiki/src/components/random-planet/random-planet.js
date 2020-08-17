d230ca37b002da9b656ba65239413407f660db18
Bananosplit <Serg-rudk@yandex.ru>
2020-08-17 12:35:37 +0300

    изменение стилизации, исправление багов

--- star-wiki/src/components/random-planet/random-planet.js
+++ star-wiki/src/components/random-planet/random-planet.js
@@ -5,9 +5,11 @@ export default function RandomPlanet() {
 
     return (
         <div className="random-planet">
+
             <div className="planet-logo">
                 <img src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
             </div>
+
             <div className='random-planet-details'>
                 <div className="name-planet"> <h3>Name planet</h3></div>
                 <div className="feature-list">
