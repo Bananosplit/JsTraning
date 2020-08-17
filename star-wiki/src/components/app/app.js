af8df408956afbb386a11d663a9814eefcaa336c
Bananosplit <Serg-rudk@yandex.ru>
2020-08-17 12:34:24 +0300

    Фича/добавил новую компаненту, обернул списки в отдельную обертку, стилизовал

--- star-wiki/src/components/app/app.js
+++ star-wiki/src/components/app/app.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+
 import Header from '../header'
 import RandomPlanet from '../random-planet'
 import PersonDetails from '../person-details'
+import ListItem from '../list-item'
+
 import './app.css'
 
 export default class App extends Component {
@@ -9,10 +12,15 @@ export default class App extends Component {
     render(){
 
         return (
+            <div className = "app-wrap">
             <div className = "app">
                 <Header/>
                 <RandomPlanet/>
-                <PersonDetails/>
+                <div className = "star-list-wrap">
+                    <ListItem/>
+                    <PersonDetails/>
+                </div>
+            </div>
             </div>
         )
     }
